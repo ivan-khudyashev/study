1d6e476668fd9915c8e185cd60136a828ea8618e
Ivan Hudyashov <ihudyashov@ptsecurity.com>
2018-08-24 18:05:52 +0700

    Fix 5 exercise in chapter 3
    5 exercise in chapter demand function for check.
    This function was added

--- lua/Ierusalimschy_3ed_book/ch03/ex05.lua
+++ lua/Ierusalimschy_3ed_book/ch03/ex05.lua
@@ -1,7 +1,10 @@
 #!/usr/bin/lua52
 -- Copyright (C) Khudyashev Ivan, 2018, bahek1983@gmail.com
 -- Learning Lua Project
-a = 10
-textA = tostring(a)
-print(textA == "false" or textA == "true")
--- print(a or (a == false))
+function isBooleanValue(val)
+    return tostring(val or (val == false)) == "true"
+end
+
+print("value a = " .. tostring(a), isBooleanValue(a))
+print("boolean value true", isBooleanValue(true))
+print("boolean value false", isBooleanValue(false))
