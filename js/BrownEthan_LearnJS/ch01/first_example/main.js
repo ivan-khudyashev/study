9337f694c55c11c30c7b1f06a206b25939f429d5
Khudyashev Ivan <bahek1983@gmail.com>
2019-03-07 18:30:53 +0700

    Make little experiments with circles in ch1

--- js/BrownEthan_LearnJS/ch01/first_example/main.js
+++ js/BrownEthan_LearnJS/ch01/first_example/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-    'use strict';
-    paper.install(window);
-    paper.setup(document.getElementById('mainCanvas'));
-    var c = Shape.Circle(200, 200, 50);
-    c.fillColor = 'green';
-    paper.view.draw();
-    //console.log('main.js loaded');
+        'use strict';
+        paper.install(window);
+        paper.setup(document.getElementById('mainCanvas'));
+        var c;
+        for(var x = 25; x < 400; x+=35) {
+                for(var y = 25; y < 400; y+=50) {
+                        c = Shape.Circle(x, y, 20);
+                        c.fillColor = 'red';
+                }
+        }
+        paper.view.draw();
+        //console.log('main.js loaded');
 });
