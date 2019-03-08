2d227ff7f97f397a1aaeaf450769d7765609be02
Khudyashev Ivan <bahek1983@gmail.com>
2019-03-08 16:08:46 +0700

    Remade test on user-click handle action

--- js/BrownEthan_LearnJS/ch01/first_example/main.js
+++ js/BrownEthan_LearnJS/ch01/first_example/main.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
         'use strict';
         paper.install(window);
         paper.setup(document.getElementById('mainCanvas'));
-        var c;
-        for(var x = 25; x < 400; x+=35) {
-                for(var y = 25; y < 400; y+=50) {
-                        c = Shape.Circle(x, y, 20);
-                        c.fillColor = 'red';
-                }
+        var tool = new Tool();
+        var c = Shape.Circle(200, 200, 80);
+        c.fillColor = 'black';
+        var text = new PointText(200, 200);
+        text.justification = 'center';
+        text.fillColor = 'white';
+        text.fontSize = 20;
+        text.content = 'hello world';
+        tool.onMouseDown = function(event) {
+                var c = Shape.Circle(event.point, 20);
+                c.fillColor = 'green';
         }
         paper.view.draw();
         //console.log('main.js loaded');
