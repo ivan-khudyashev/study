1b39983211d989435f3ba68985e7d5f05228c7ed
Khudyashev Ivan <bahek1983@gmail.com>
2019-04-30 12:22:34 +0700

    Add safety in pattern of bash-script

--- bash/pattern.sh
+++ bash/pattern.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Copyright (C) Khudyashev Ivan, 2019, bahek1983@gmail.com
+set -o nounset # Save of using not defined variables
+set -o errexit # Save from ignoring error-exit of commands
 main(){
     printf "Start script\n"
     printf "End script\n"
