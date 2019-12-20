12d1d48165b240d9c2fbe13ba5ea6d00d7ae9db4
Khudyashev Ivan <bahek1983@gmail.com>
2019-12-20 20:40:22 +0700

    Finish ch01 Lubanovic "Introducing Python"

---
+++ python3/LubanovicBill_IntroPython/ch01/t1.2.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+PYTHON_BIN=python3
+echo "Start python3"
+${PYTHON_BIN}
---
+++ python3/LubanovicBill_IntroPython/ch01/t1.4.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2019, Khudyashev Ivan, bahek1983@gmail.com
+
+def doAction():
+    print("Here just const 47 in next statement")
+    47
+
+def main():
+    doAction()
+
+if __name__ == "__main__":
+    main()
