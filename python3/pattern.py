a1f4b280c26c70bdf40230817e61b7c4af549819
Khudyashev Ivan <bahek1983@gmail.com>
2021-10-22 20:50:32 +0700

    Make pattern.py more pythonic

--- python3/pattern.py
+++ python3/pattern.py
@@ -1,10 +1,13 @@
 # Copyright (C) 2019-2021, Khudyashev Ivan, bahek1983@gmail.com
 
+
 def doAction():
     print("Test program")
 
+
 def main():
     doAction()
+
 
 if __name__ == "__main__":
     main()
